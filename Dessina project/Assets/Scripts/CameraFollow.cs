2b033bdd5d78ac16b2798abc0ad42cac79ee0c30
dotcom4143 <>
2026-03-19 17:55:36 +0900

    뱀파이어 서바이벌 라이크 제작

---
+++ Dessina project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+    public Vector3 offset = new Vector3 (0f, 12f, -8f);
+
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+        transform.position = target.position + offset;
+    }
+}
